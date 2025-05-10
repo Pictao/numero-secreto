5844775e4de0a81723e995eddd7a3a0f3d1cbb81
Pictao <pictao69@gmail.com>
2025-05-10 19:10:43 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroMax = 10;
+let numeroMax = 100;
 let tentativas = 1;
 let numeroSecreto = gerarNumeroAleatorio();
 
@@ -35,7 +35,7 @@ function verificarChute() {
 
 function exibirMsgInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function reiniciarJogo() {
